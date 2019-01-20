a23c9014b29c19a5a4c5d0c5b583df1e244370f1
zoulu <545926080@qq.com>
2019-01-21 00:35:52 +0800

    改进

--- api-gateway/src/main/java/com/xiaopotian/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/xiaopotian/apigateway/ApiGatewayApplication.java
@@ -2,10 +2,13 @@ package com.xiaopotian.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
 @EnableZuulProxy
+//@EnableDiscoveryClient跟EnableEurekaClient用哪个都可以
+@EnableEurekaClient
 public class ApiGatewayApplication {
 
     public static void main(String[] args) {
